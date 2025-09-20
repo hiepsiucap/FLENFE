253e86073e1b049d49228bcfb726b5a308bf136c
ronaldhiepgoldenown <ronald.nguyen.goldenowl@gmail.com>
2025-09-21 01:47:37 +0700

    Update Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  assetsInclude: ['**/*.tsx', '**/*.ts'],
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.[tj]sx?$/,
+    exclude: []
+  }
 })
